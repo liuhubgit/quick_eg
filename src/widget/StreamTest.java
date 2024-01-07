7dd64bb083428bee074f68095dfd1681cec636b3
liuhubgit <chengjinliu@126.com>
2024-01-07 17:45:00 +0800

    first main class

---
+++ src/widget/StreamTest.java
@@ -0,0 +1,11 @@
+package widget;
+public class StreamTest {
+    public static void main(String[] args) {
+        System.out.println("run");
+    }
+
+    public void doStream() {
+        System.out.println("start");
+    }
+
+}
